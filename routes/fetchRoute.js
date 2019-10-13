66a014020e4aed6f688945a10a67290ea5b55dcc
root <root@DESKTOP-1ML003O.localdomain>
2019-10-14 10:04:51 +1100

    updated backend fetch

--- routes/fetchRoute.js
+++ routes/fetchRoute.js
@@ -4,19 +4,23 @@ const axios = require('axios')
 require('dotenv').config()
 
 router.get('/', async (req, res) => {
+    try {
+        const photoResponse = await axios.get(`https://api.unsplash.com/photos/random/?client_id=${process.env.PHOTO}`)
+        const photo = photoResponse.data
     
-    const photoResponse = await axios.get(`https://api.unsplash.com/photos/random/?client_id=${process.env.PHOTO}`)
-    const photo = photoResponse.data
-
-    const factResponse = await axios.get("https://uselessfacts.jsph.pl/random.json?language=en")
-    const fact = factResponse.data
-
-    const data = {
-        fact: fact.text,
-        photo: photo.urls.small
-    };
-
-    res.send(data)
+        const factResponse = await axios.get("https://uselessfacts.jsph.pl/random.json?language=en")
+        const fact = factResponse.data
+    
+        const data = {
+            fact: fact.text,
+            photo: photo.urls.small
+        };
+    
+        res.send(data)
+    } catch (error) {
+        console.log(error)
+    }
+  
 });
 
 module.exports = router;
